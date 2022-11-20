3214c9dbcc51c410c46cf60d15fa8469bfa7f3a4
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2022-11-20 13:20:20 +0000

    publishing image

--- webapp/creation.sql
+++ webapp/creation.sql
@@ -13,34 +13,33 @@
 -----------------------------------------
 
 DROP SCHEMA IF EXISTS offtheshelf CASCADE;
-
 CREATE SCHEMA offtheshelf;
+SET search_path TO offtheshelf;
 
-DROP TABLE IF EXISTS offtheshelf.faq CASCADE;
-DROP TABLE IF EXISTS offtheshelf.admins CASCADE;
-DROP TABLE IF EXISTS offtheshelf.publisher CASCADE;
-DROP TABLE IF EXISTS offtheshelf.author CASCADE;
-DROP TABLE IF EXISTS offtheshelf.collections CASCADE;
-DROP TABLE IF EXISTS offtheshelf.category CASCADE;
-DROP TABLE IF EXISTS offtheshelf.users CASCADE;
-DROP TABLE IF EXISTS offtheshelf.book CASCADE;
-DROP TABLE IF EXISTS offtheshelf.wishlist CASCADE;
-DROP TABLE IF EXISTS offtheshelf.cart CASCADE;
-DROP TABLE IF EXISTS offtheshelf.purchase CASCADE;
-DROP TABLE IF EXISTS offtheshelf.photo CASCADE;
-DROP TABLE IF EXISTS offtheshelf.book_author CASCADE;
-DROP TABLE IF EXISTS offtheshelf.book_collection CASCADE;
-DROP TABLE IF EXISTS offtheshelf.review CASCADE;
-DROP TABLE IF EXISTS offtheshelf.purchase_book CASCADE;
-DROP TABLE IF EXISTS offtheshelf.delivery CASCADE;
+DROP TABLE IF EXISTS faq CASCADE;
+DROP TABLE IF EXISTS publisher CASCADE;
+DROP TABLE IF EXISTS author CASCADE;
+DROP TABLE IF EXISTS collections CASCADE;
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS book CASCADE;
+DROP TABLE IF EXISTS wishlist CASCADE;
+DROP TABLE IF EXISTS cart CASCADE;
+DROP TABLE IF EXISTS purchase CASCADE;
+DROP TABLE IF EXISTS photo CASCADE;
+DROP TABLE IF EXISTS book_author CASCADE;
+DROP TABLE IF EXISTS book_collection CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS purchase_book CASCADE;
+DROP TABLE IF EXISTS delivery CASCADE;
 
-DROP TYPE IF EXISTS offtheshelf.purchase_state CASCADE;
+DROP TYPE IF EXISTS purchase_state CASCADE;
 
 -----------------------------------------
 -- Types
 -----------------------------------------
 
-CREATE TYPE offtheshelf.purchase_state AS ENUM ('Received', 'Dispatched', 'Delivered');
+CREATE TYPE purchase_state AS ENUM ('Received', 'Dispatched', 'Delivered');
 
 -----------------------------------------
 -- Tables
@@ -48,51 +47,45 @@ CREATE TYPE offtheshelf.purchase_state AS ENUM ('Received', 'Dispatched', 'Deliv
 
 -- Note that some plural names were adopted because of reserved words in PostgreSQL.
 
-CREATE TABLE offtheshelf.faq (
+CREATE TABLE faq (
     question TEXT,
     answer TEXT,
     PRIMARY KEY (question, answer)
 );
 
-CREATE TABLE offtheshelf.admins (
-    id_admin SERIAL PRIMARY KEY,
-    admin_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    admin_password TEXT NOT NULL
+CREATE TABLE publisher (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE offtheshelf.publisher (
-    id_publisher SERIAL PRIMARY KEY,
-    publisher_name TEXT NOT NULL
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE offtheshelf.author (
-    id_author SERIAL PRIMARY KEY,
-    author_name TEXT NOT NULL
+CREATE TABLE collections (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE offtheshelf.collections (
-    id_collection SERIAL PRIMARY KEY,
-    collection_name TEXT NOT NULL
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE offtheshelf.category (
-    id_category SERIAL PRIMARY KEY,
-    category_name TEXT NOT NULL
-);
-
-CREATE TABLE offtheshelf.users (
-    id_user SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,      
-    user_password TEXT NOT NULL,                  
+    password TEXT NOT NULL,                  
     user_address TEXT,
     phone CHAR(9),
-    blocked BOOLEAN DEFAULT FALSE NOT NULL            
+    blocked BOOLEAN DEFAULT FALSE NOT NULL,
+    admin_perms BOOLEAN DEFAULT FALSE NOT NULL     
 );
 
-CREATE TABLE offtheshelf.book (
-    id_book SERIAL PRIMARY KEY,
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,      
     isbn NUMERIC NOT NULL UNIQUE,
     year INTEGER,  
@@ -100,87 +93,86 @@ CREATE TABLE offtheshelf.book (
     stock INTEGER NOT NULL CHECK (stock >= 0),           
     book_edition INTEGER,
     book_description TEXT,
-    id_category INTEGER NOT NULL REFERENCES offtheshelf.category(id_category) ON UPDATE CASCADE ON DELETE CASCADE,
-    id_publisher INTEGER REFERENCES offtheshelf.publisher(id_publisher) ON UPDATE CASCADE ON DELETE CASCADE                         
+    category_id INTEGER NOT NULL REFERENCES category(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    publisher_id INTEGER REFERENCES publisher(id) ON UPDATE CASCADE ON DELETE CASCADE                         
 );
 
-CREATE TABLE offtheshelf.wishlist (
-    id_user INTEGER REFERENCES offtheshelf.users(id_user) ON DELETE CASCADE,
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON DELETE CASCADE,
-    PRIMARY KEY (id_user, id_book)        
+CREATE TABLE wishlist (
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, book_id)        
 );
 
-CREATE TABLE offtheshelf.cart (
-    id_user INTEGER REFERENCES offtheshelf.users(id_user) ON DELETE CASCADE,
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON DELETE CASCADE,
-    PRIMARY KEY (id_user, id_book)        
+CREATE TABLE cart (
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, book_id)        
 );
 
-CREATE TABLE offtheshelf.purchase (
-    id_purchase SERIAL PRIMARY KEY,
+CREATE TABLE purchase (
+    id SERIAL PRIMARY KEY,
     purchase_date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-    id_user INTEGER NOT NULL REFERENCES offtheshelf.users(id_user) ON DELETE CASCADE,
-    state_purchase offtheshelf.purchase_state NOT NULL
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    state_purchase purchase_state NOT NULL
 );
 
-CREATE TABLE offtheshelf.photo (
-    id_photo SERIAL PRIMARY KEY,
+CREATE TABLE photo (
+    id SERIAL PRIMARY KEY,
     photo_image TEXT NOT NULL,      
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON UPDATE CASCADE ON DELETE CASCADE,
-    id_user INTEGER UNIQUE REFERENCES offtheshelf.users(id_user) ON UPDATE CASCADE ON DELETE CASCADE,
-    id_admin INTEGER UNIQUE REFERENCES offtheshelf.admins(id_admin) ON UPDATE CASCADE ON DELETE CASCADE           
+    book_id INTEGER REFERENCES book(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    user_id INTEGER UNIQUE REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE      
 );
 
-CREATE TABLE offtheshelf.book_author (
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON DELETE CASCADE,
-    id_author INTEGER REFERENCES offtheshelf.author(id_author) ON DELETE CASCADE,
-    PRIMARY KEY (id_book, id_author)                       
+CREATE TABLE book_author (
+    book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+    author_id INTEGER REFERENCES author(id) ON DELETE CASCADE,
+    PRIMARY KEY (book_id, author_id)                       
 );
 
-CREATE TABLE offtheshelf.book_collection (
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON DELETE CASCADE,
-    id_collection INTEGER REFERENCES offtheshelf.collections(id_collection) ON DELETE CASCADE,
-    PRIMARY KEY (id_book, id_collection)                 
+CREATE TABLE book_collection (
+    book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+    collection_id INTEGER REFERENCES collections(id) ON DELETE CASCADE,
+    PRIMARY KEY (book_id, collection_id)                 
 );
 
-CREATE TABLE offtheshelf.review (
-    id_review SERIAL PRIMARY KEY,
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
     rating INTEGER NOT NULL CHECK (rating >= 0 AND rating <= 5),
     comment TEXT,
     review_date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-    id_book INTEGER NOT NULL REFERENCES offtheshelf.book(id_book) ON UPDATE CASCADE ON DELETE CASCADE,
-    id_user INTEGER NOT NULL REFERENCES offtheshelf.users(id_user) ON UPDATE CASCADE ON DELETE CASCADE          
+    book_id INTEGER NOT NULL REFERENCES book(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE          
 );
 
-CREATE TABLE offtheshelf.purchase_book (
-    id_purchase INTEGER REFERENCES offtheshelf.purchase(id_purchase) ON DELETE CASCADE,
-    id_book INTEGER REFERENCES offtheshelf.book(id_book) ON DELETE CASCADE,
-    PRIMARY KEY (id_purchase, id_book)        
+CREATE TABLE purchase_book (
+    purchase_id INTEGER REFERENCES purchase(id) ON DELETE CASCADE,
+    book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+    PRIMARY KEY (purchase_id, book_id)        
 );
 
-CREATE TABLE offtheshelf.delivery (
-    id_delivery SERIAL PRIMARY KEY,
+CREATE TABLE delivery (
+    id SERIAL PRIMARY KEY,
     arrival TIMESTAMP WITH TIME ZONE NOT NULL,
     delivery_address TEXT NOT NULL,
     cost NUMERIC(9, 2) NOT NULL CHECK (cost >= 0),
-    id_purchase INTEGER NOT NULL UNIQUE REFERENCES offtheshelf.purchase(id_purchase) ON UPDATE CASCADE ON DELETE CASCADE   
+    purchase_id INTEGER NOT NULL UNIQUE REFERENCES purchase(id) ON UPDATE CASCADE ON DELETE CASCADE   
 );
 
 -----------------------------------------
 -- INDEXES
 -----------------------------------------
 
-CREATE INDEX user_purchase ON offtheshelf.purchase USING hash (id_user);
+CREATE INDEX user_purchase ON purchase USING hash (user_id);
 
-CREATE INDEX book_category ON offtheshelf.book USING btree (id_category);
-CLUSTER offtheshelf.book USING book_category;
+CREATE INDEX book_category ON book USING btree (category_id);
+CLUSTER book USING book_category;
 
-CREATE INDEX book_review ON offtheshelf.review USING hash (id_book);
+CREATE INDEX book_review ON review USING hash (book_id);
 
 -- FTS INDEXES
 
 -- Add column to book to store computed ts_vectors.
-ALTER TABLE offtheshelf.book
+ALTER TABLE book
 ADD COLUMN tsvectors TSVECTOR;
 
 -- Create a function to automatically update ts_vectors.
@@ -204,13 +196,13 @@ LANGUAGE plpgsql;
 
 -- Create a trigger before insert or update on book.
 CREATE TRIGGER book_search_update
- BEFORE INSERT OR UPDATE ON offtheshelf.book
+ BEFORE INSERT OR UPDATE ON book
  FOR EACH ROW
  EXECUTE PROCEDURE book_search_update();
 
 
 -- Finally, create a GIN index for ts_vectors.
-CREATE INDEX search_idx ON offtheshelf.book USING GIN (tsvectors);
+CREATE INDEX search_idx ON book USING GIN (tsvectors);
 
 -----------------------------------------
 -- TRIGGERS and UDFs
@@ -222,7 +214,7 @@ CREATE INDEX search_idx ON offtheshelf.book USING GIN (tsvectors);
 CREATE OR REPLACE FUNCTION book_available() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        IF EXISTS (SELECT * FROM offtheshelf.book WHERE id_book = NEW.id_book AND stock = 0) THEN
+        IF EXISTS (SELECT * FROM book WHERE id = NEW.book_id AND stock = 0) THEN
            RAISE EXCEPTION 'This book is out of stock.';
         END IF;
         RETURN NEW;
@@ -231,7 +223,7 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER book_available
-        BEFORE INSERT ON offtheshelf.purchase_book
+        BEFORE INSERT ON purchase_book
         FOR EACH ROW
         EXECUTE PROCEDURE book_available();
 
@@ -241,15 +233,16 @@ CREATE TRIGGER book_available
 CREATE OR REPLACE FUNCTION book_purchased() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        UPDATE offtheshelf.book
+        UPDATE book
         SET stock = stock - 1
-        WHERE "id_book" = NEW."id_book";
+        WHERE id = NEW.book_id;
+        RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER book_purchased
-        AFTER INSERT ON offtheshelf.purchase_book
+        AFTER INSERT ON purchase_book
         EXECUTE PROCEDURE book_purchased();
 
 -- TRIGGER03
@@ -258,15 +251,16 @@ CREATE TRIGGER book_purchased
 CREATE OR REPLACE FUNCTION wishlist_to_cart() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        DELETE FROM offtheshelf.wishlist
-        WHERE "id_user" = NEW."id_user"
-        AND "id_book" = NEW."id_book";
+        DELETE FROM wishlist
+        WHERE user_id = NEW.user_id
+        AND book_id = NEW.book_id;
+        RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER wishlist_to_cart 
-        AFTER INSERT ON offtheshelf.cart
+        AFTER INSERT ON cart
         EXECUTE PROCEDURE wishlist_to_cart();
 
 -- TRIGGER04
@@ -275,14 +269,15 @@ CREATE TRIGGER wishlist_to_cart
 CREATE OR REPLACE FUNCTION cart_purchased() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        DELETE FROM offtheshelf.cart
-        WHERE "id_user" = NEW."id_user";
+        DELETE FROM cart
+        WHERE user_id = NEW.user_id;
+        RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER cart_purchased 
-        AFTER INSERT ON offtheshelf.purchase
+        AFTER INSERT ON purchase
         EXECUTE PROCEDURE cart_purchased();
 
 -- TRIGGER05
@@ -291,7 +286,7 @@ CREATE TRIGGER cart_purchased
 CREATE OR REPLACE FUNCTION blocked_review() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        IF EXISTS (SELECT * FROM offtheshelf.users WHERE id_user = NEW.id_user AND blocked = TRUE) THEN
+        IF EXISTS (SELECT * FROM users WHERE id = NEW.id AND blocked = TRUE) THEN
            RAISE EXCEPTION 'Blocked users cannot submit reviews.';
         END IF;
         RETURN NEW;
@@ -300,7 +295,7 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER blocked_review
-        BEFORE INSERT ON offtheshelf.review
+        BEFORE INSERT ON review
         FOR EACH ROW
         EXECUTE PROCEDURE blocked_review();
 
@@ -310,7 +305,7 @@ CREATE TRIGGER blocked_review
 CREATE OR REPLACE FUNCTION blocked_purchase() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-        IF EXISTS (SELECT * FROM offtheshelf.users WHERE id_user = NEW.id_user AND blocked = TRUE) THEN
+        IF EXISTS (SELECT * FROM users WHERE id = NEW.id AND blocked = TRUE) THEN
            RAISE EXCEPTION 'Blocked users cannot purchase books.';
         END IF;
         RETURN NEW;
@@ -319,7 +314,7 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER blocked_purchase
-        BEFORE INSERT ON offtheshelf.purchase
+        BEFORE INSERT ON purchase
         FOR EACH ROW
         EXECUTE PROCEDURE blocked_purchase();
 
