453752b035e7231bc260377401f89366067d3233
P1ntas <72892065+P1ntas@users.noreply.github.com>
2022-11-21 14:53:56 +0000

    Minor fix in login

--- design/javascript/login.js
+++ design/javascript/login.js
@@ -6,10 +6,14 @@ function register() {
     x.style.left = "-28.125em";
     y.style.left = "6.25em";
     z.style.left = "5.625em";
+    document.getElementById("log1").style.color = "white";
+    document.getElementById("log2").style.color = "black";
 }
 
 function login() {
     x.style.left = "6.25em";
     y.style.left = "31.25em";
     z.style.left = "0em";
+    document.getElementById("log2").style.color = "white";
+    document.getElementById("log1").style.color = "black";
 }
