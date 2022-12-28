72891b213e6ed979a5f9488c5f977c84288bd24e
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2022-12-28 04:34:47 +0000

    users branch

---
+++ webapp/app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    // to do
+}
